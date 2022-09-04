973e9141220c6dcaa7852be4427f424ad904b7ef
«Dmitriy» <arsenal_lion@mail.ru>
2022-09-04 13:07:02 +0300

    Tasks made

---
+++ Tasks/Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму цифр числа
+int SumDigits(int number)
+{
+    if (number < 1) return 0;
+    return SumDigits(number / 10) + number % 10;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа: {SumDigits(number)}");
